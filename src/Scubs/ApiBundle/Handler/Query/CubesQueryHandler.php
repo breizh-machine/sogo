b788f6f3014766ad5084b43bb93673bdd93539bf
FMARTIN <francois.martin.fcb@gmail.com>
2016-06-15 17:21:56 +0200

    Modified cubes query handler to generate images path

--- src/Scubs/ApiBundle/Handler/Query/CubesQueryHandler.php
+++ src/Scubs/ApiBundle/Handler/Query/CubesQueryHandler.php
@@ -4,18 +4,38 @@ namespace Scubs\ApiBundle\Handler\Query;
 
 use Scubs\CoreDomain\Cube\CubeRepository;
 use Scubs\ApiBundle\Query\CubesQuery;
+use Scubs\ApiBundle\View\CubeView;
+use Symfony\Component\Asset\Packages;
+use Doctrine\Common\Collections\ArrayCollection;
 
 class CubesQueryHandler
 {
     private $cubeRepository;
+    private $assetsHelper;
 
-    public function __construct(CubeRepository $cubeRepository)
+    public function __construct(CubeRepository $cubeRepository, Packages $assetsHelper)
     {
         $this->cubeRepository = $cubeRepository;
+        $this->assetsHelper = $assetsHelper;
     }
 
     public function handle(CubesQuery $query)
     {
-        return $this->cubeRepository->findAll();
+        $imagesBasePath = 'bundles/scubscoredomain/cube/images';
+        
+        $allCubes = $this->cubeRepository->findAll();
+        $allCubeViews = new ArrayCollection();
+        foreach ($allCubes as $cube)
+        {
+            $cubeView = new CubeView();
+            $cubeView->id = (string) $cube->getId();
+            $cubeView->description = $cube->getDescription();
+            $cubeView->rarity = $cube->getRarity();
+            $cubeView->textureUrl = $this->assetsHelper->getUrl(sprintf('%s/%s', $imagesBasePath, $cube->getTexture()));
+            $cubeView->thumbnailUrl = $this->assetsHelper->getUrl(sprintf('%s/%s', $imagesBasePath, $cube->getThumbnail()));
+            $allCubeViews->add($cubeView);
+        }
+
+        return $allCubeViews;
     }
 }
---
+++ src/Scubs/ApiBundle/View/CubeView.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Scubs\ApiBundle\View;
+
+class CubeView
+{
+    public $id;
+    public $textureUrl;
+    public $thumbnailUrl;
+    public $description;
+    public $rarity;
+}
